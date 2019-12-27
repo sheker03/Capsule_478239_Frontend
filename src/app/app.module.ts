828cadde086c5774443bc03039760dbcbc5af9c2
sheker03 <sheker01@gmail.com>
2019-12-27 10:57:08 +0530

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './ui/add/add.component';
+import { EditComponent } from './ui/edit/edit.component';
+import { ViewComponent } from './ui/view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    EditComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
